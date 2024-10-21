6672a1f6eecf01a775a717dbc10c11094883d4d1
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-10-21 12:54:59 +0900

    [Feature]_QuestManager
    [구현사항 1] 정적 필드 정의

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    private static QuestManager instance;
+    //[구현사항 1] 정적 필드 정의
+}
