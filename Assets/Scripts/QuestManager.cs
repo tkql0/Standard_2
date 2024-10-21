3a8288f5371c1c2a305008d27bb283f81f52fc0c
tkql0 <96943505+tkql0@users.noreply.github.com>
2024-10-21 12:56:04 +0900

    [Feature]_QuestManager
    // [구현사항 2] 정적 프로퍼티 정의

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -6,4 +6,19 @@ public class QuestManager : MonoBehaviour
 {
     private static QuestManager instance;
     //[구현사항 1] 정적 필드 정의
+
+    public static QuestManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<QuestManager>();
+
+            if (instance == null)
+                instance = new GameObject("QuestManager").AddComponent<QuestManager>();
+
+            return instance;
+        }
+    }
+    // [구현사항 2] 정적 프로퍼티 정의
 }
